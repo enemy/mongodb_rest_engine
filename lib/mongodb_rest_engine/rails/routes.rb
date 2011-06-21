df81149635ac126135aaae9301db3f2f7bd33064
Olli Jokinen <olli.jokinen@enemy.fi>
2011-06-21 20:57:39 +0300

    customizable controller names @ routes.rb

--- lib/mongodb_rest_engine/rails/routes.rb
+++ lib/mongodb_rest_engine/rails/routes.rb
@@ -1,12 +1,20 @@
 module ActionDispatch::Routing
   class Mapper
-    def mongo_datastore_for(namespace_name)
+    def mongo_datastore_for(namespace_name, *resources)
+      options = resources.extract_options!
+
+      if options[:controllers] and options[:controllers][:mongodb]
+        mongodb_controller = options[:controllers][:mongodb] 
+      else
+        mongodb_controller = "mongodb_rest_engine/mongodb"
+      end
+
       scope "/#{namespace_name}" do
-        get ':collection', :controller => "mongodb_rest_engine/mongodb", :action => "index"
-        get ':collection/:id', :controller => "mongodb_rest_engine/mongodb", :action => "show"
-        post ':collection', :controller => "mongodb_rest_engine/mongodb", :action => "create"
-        put ':collection/:id', :controller => "mongodb_rest_engine/mongodb", :action => "update"
-        delete ':collection/:id', :controller => "mongodb_rest_engine/mongodb", :action => "destroy"
+        get ':collection', :controller => mongodb_controller, :action => "index"
+        get ':collection/:id', :controller => mongodb_controller, :action => "show"
+        post ':collection', :controller => mongodb_controller, :action => "create"
+        put ':collection/:id', :controller => mongodb_controller, :action => "update"
+        delete ':collection/:id', :controller => mongodb_controller, :action => "destroy"
       end
     end
   end
